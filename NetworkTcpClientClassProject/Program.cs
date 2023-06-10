ca305fd5f1434bdc05a347585e83f4d474f77e81
maxefimov <efimov.top.academy@gmail.com>
2023-06-10 17:26:17 +0300

    Добавьте файлы проекта.

---
+++ NetworkTcpClientClassProject/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+/*
+using TcpClient tcpClient = new TcpClient();
+
+string url = "yandex.ru";
+int port = 80;
+
+try
+{
+    await tcpClient.ConnectAsync(url, port);
+    Console.WriteLine("Connected!");
+    NetworkStream netStream = tcpClient.GetStream();
+
+    string request = $"GET / HTTP/1.1\r\nHost: {url}\r\nConnection: Close\r\n\r\n";
+    byte[] requestBuffer = Encoding.UTF8.GetBytes(request);
+    await netStream.WriteAsync(requestBuffer, 0, requestBuffer.Length);
+
+    byte[] responseBuffer = new byte[1024];
+    StringBuilder response = new StringBuilder();
+
+    //int bytesRead;
+    do
+    {
+        await netStream.ReadAsync(responseBuffer);
+        response.Append(Encoding.UTF8.GetString(responseBuffer));
+    } while (netStream.DataAvailable);
+
+    Console.WriteLine(response);
+}
+catch(SocketException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+*/
+
+using TcpClient client = new TcpClient();
+Console.WriteLine("Client start!");
+await client.ConnectAsync(IPAddress.Loopback, 8888);
+
+if(client.Connected)
+    Console.WriteLine($"Connect to server: {client.Client.RemoteEndPoint}");
+
+byte[] buffer = new byte[1024];
+NetworkStream stream = client.GetStream();
+
+int bytes = await stream.ReadAsync(buffer);
+Console.WriteLine($"Message from server: {Encoding.UTF8.GetString(buffer)}");
+
+//client.Client.Shutdown(SocketShutdown.Both);
+
+/*
+string message = "Hello server! " + DateTime.Now.ToLongTimeString();
+buffer = Encoding.UTF8.GetBytes(message);
+
+await stream.WriteAsync(buffer);
+Console.WriteLine($"Message to server {message} sended");
+*/
+
+string[] messages = new[] { "First message", "Second message", "Other message" };
+
+Console.WriteLine();
+
+var streamWriter = client.GetStream();
+
+foreach (string mess in messages)
+{
+    byte[] bufferMess = Encoding.UTF8.GetBytes(mess + "$");
+    await streamWriter.WriteAsync(bufferMess);
+    Console.WriteLine($"Multisend. Message {mess} sended");
+}
+
+await stream.WriteAsync(Encoding.UTF8.GetBytes("END$"));
---
+++ NetworkTcpListenerClassProject/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+//IPAddress localAddress = IPAddress.Parse("127.0.0.1");
+IPAddress localAddress = IPAddress.Loopback;
+int localPort = 8888;
+
+IPEndPoint localEndPoint = new IPEndPoint(localAddress, localPort);
+
+//TcpListener listener = new TcpListener(localAddress, localPort);
+TcpListener listener = new TcpListener(localEndPoint);
+
+try
+{
+    listener.Start();
+    Console.WriteLine("Server start!");
+
+    while(true)
+    {
+        using TcpClient client = await listener.AcceptTcpClientAsync();
+        //using Socket socketClient = await listener.AcceptSocketAsync();
+        Console.WriteLine($"Remote client: {client.Client.RemoteEndPoint}");
+        //Console.WriteLine($"Remote client: {socketClient.RemoteEndPoint}");
+        NetworkStream stream = client.GetStream();
+
+        string message = "Hello client! " + DateTime.Now.ToLongTimeString();
+
+        
+        byte[] buffer = Encoding.UTF8.GetBytes(message);
+        await stream.WriteAsync(buffer);
+        Console.WriteLine($"Message to client: {message} sended");
+        /*
+        StringBuilder response = new StringBuilder();
+
+        do
+        {
+            buffer = new byte[1024];
+            await stream.ReadAsync(buffer);
+            response.Append(Encoding.UTF8.GetString(buffer));
+        } while (stream.DataAvailable);
+
+        Console.WriteLine($"Message from client {response.ToString()} received");
+        */
+        List<byte> bufferList = new List<byte>();
+        int bufferByte;
+
+        while(true)
+        {
+            while ((bufferByte = stream.ReadByte()) != '$')
+                bufferList.Add((byte)bufferByte);
+
+            string mess = Encoding.UTF8.GetString(bufferList.ToArray());
+            if (mess == "END") break;
+            Console.WriteLine($"MultiReceive. Message {mess} received");
+            bufferList.Clear();
+        }
+        
+
+
+    }
+    
+}
+finally
+{
+    listener.Stop();
+}
